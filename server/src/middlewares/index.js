4cf9a873f27292e0aed7bcefab73adb6f1f8c4f7
Camilo <imdrizzky@gmail.com>
2025-04-07 22:55:59 +0200

    Killed everything

--- server/src/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-import authUserMiddleware from './authUserMiddleware.js';
-import postExistsMiddleware from './postExistsMiddleware.js';
-import canEditPostMiddleware from './canEditPostMiddleware.js';
-
-export { authUserMiddleware, postExistsMiddleware, canEditPostMiddleware };
--- server/src/utils/generateErrorUtil.js
+++
@@ -1,7 +0,0 @@
-const generateErrorUtil = (msg, code) => {
-    const err = new Error(msg);
-    err.httpStatus = code;
-    throw err;
-};
-
-export default generateErrorUtil;
