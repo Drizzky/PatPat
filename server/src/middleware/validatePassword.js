9f5276010c7952f96f154eda1a796f5f2d0dae8c
Camilo <imdrizzky@gmail.com>
2025-08-13 18:12:46 +0200

    Changed pattern for validate password

--- server/src/middleware/validatePassword.js
+++ server/src/middleware/validatePassword.js
@@ -4,7 +4,7 @@ const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
   // One capital, one number, one symbol
-  const pattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+  const pattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,18}$/;
 
   if (!pattern.test(password)) {
     return next(throwError(400, 'Password must contain at least one uppercase letter, one number, and one special character.'));
