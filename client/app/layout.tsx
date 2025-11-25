5c741c6de8ed165c06f9eff7bd19dd0030deccc8
Camilo <imdrizzky@gmail.com>
2025-11-25 11:00:53 +0100

    ResgisterPage and more UI
    also changed dbscript to ignore fk checks

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import '@/app/globals.css';
 import Footer from '../components/ui/FooterComponent';
+import { Toaster } from '@/components/ui/sonner';
 
 export const metadata: Metadata = {
     title: 'Pat²',
@@ -18,6 +19,7 @@ export default function RootLayout({
                 <main className='flex-1 flex items-center justify-center'>
                     {children}
                 </main>
+                <Toaster />
                 <Footer />
             </body>
         </html>
